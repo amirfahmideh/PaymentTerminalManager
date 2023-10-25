6cbc40820c28e84ac40c4e841e35657915ccf6c1
Amir Fahmideh <amir.fahmideh@gmail.com>
2023-10-25 12:53:38 +0330

    factore now support Sadad_BluePrint

--- paymentTerminalManager/factory/TransactionOperationFactory.cs
+++ paymentTerminalManager/factory/TransactionOperationFactory.cs
@@ -8,6 +8,9 @@ internal class TransactionOperationFactory {
         switch (terminalType) {
             case SupportedTerminal.BEHPARDAKHT : 
                 return new BehpardakhtTransactionOperation();
+            case SupportedTerminal.SADAD_BLUEBIRD : {
+                return new SadadBluebirdTransactionOperation();
+            }
             default:               
                 throw new TerminalNotImplementException($"ترمینال از نوع {terminalType} پیاده سازی نشده است");
         }
