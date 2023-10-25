7785fdf813d804404ffb87061710a6b4d1ee2aa5
Amir Fahmideh <amir.fahmideh@gmail.com>
2023-10-25 13:51:17 +0330

    package now support Sadad blue bird on lan connection

--- paymentTerminalManager/implement/SadadBluebirdTransactionOperation.cs
+++ paymentTerminalManager/implement/SadadBluebirdTransactionOperation.cs
@@ -23,18 +23,19 @@ namespace PaymentTerminalManager.implement
             if(terminalResult.ResponseCode == "00" ) {
                 result.IsSuccess = true;
                 result.Price = Decimal.TryParse(terminalResult.Amount, out decimal trsPrice) ? trsPrice : 0;
-                result.AccountNo = terminalResult.OrderId;
+                result.AccountNo = terminalResult.SaleId;
                 result.CardNumber = terminalResult.CardNo;
                 result.TransactionSerialNumber = terminalResult.Rrn;
                 result.TerminalNo = terminalResult.TerminalId;
                 result.ErrorCode = terminalResult.ResponseCode;
                 result.ErrorTitle = terminalResult.ResponseCodeMessage;
                 result.TransactionDateTime = DateTimeConvert.ParsPersianDateTime(terminalResult.TransactionDate, terminalResult.TransactionTime);
+                result.ReferenceNumber = terminalResult.MerchantId;
             }
             else{
 
                 result.IsSuccess = false;
-                result.ErrorCode = terminalResult.ResponseCode;
+                result.ErrorCode = terminalResult.PcPosStatusCode.ToString();
                 result.ErrorTitle = terminalResult.PcPosStatus;
             }
            
