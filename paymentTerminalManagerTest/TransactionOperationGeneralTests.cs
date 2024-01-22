93edfa9c29cf5c58214a89bb98dba75793cb02b0
Amir Fahmideh <amir.fahmideh@gmail.com>
2024-01-22 09:07:11 +0330

    update transaction to support transaction operation

--- paymentTerminalManagerTest/TransactionOperationGeneralTests.cs
+++ paymentTerminalManagerTest/TransactionOperationGeneralTests.cs
@@ -17,20 +17,20 @@ namespace PaymentTerminalManagerTest
         }
 
 
-        [Test]
-        public async Task PosRefundTest()
-        {
-            PaymentTerminalManager.TransactionOperation transactionOperation = new PaymentTerminalManager.TransactionOperation();
-            await transactionOperation.RefundRequest(SupportedTerminal.BEHPARDAKHT, new PaymentTerminalManager.dto.RefundFromTerminal()
-            {
-                Password = "54075867",
-                RefundPrice = 50000,
-                SaleReferenceId = 263072438688,
-                TerminalId = 6520823,
-                UserName = "land1401"
-            });
-            Assert.IsTrue(true);
-        }
+        //[Test]
+        //public async Task PosRefundTest()
+        //{
+        //    PaymentTerminalManager.TransactionOperation transactionOperation = new PaymentTerminalManager.TransactionOperation();
+        //    var req = await transactionOperation.RefundRequest(SupportedTerminal.BEHPARDAKHT, new PaymentTerminalManager.dto.RefundFromTerminal()
+        //    {
+        //        Password = "54075867",
+        //        RefundPrice = 50000,
+        //        SaleReferenceId = 263072438688,
+        //        TerminalId = 6520823,
+        //        UserName = "land1401"
+        //    });
+        //    Assert.IsTrue(true);
+        //}
 
 
         //[Test]
